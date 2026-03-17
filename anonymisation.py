786a3126e924132cabf824caa9c2185400c4ca72
Clément LATHIERE <clement.lathiere22@outlook.fr>
2026-03-17 12:22:41 +0100

    ajout anonymisation.py et HRDataset_v14-1.csv

---
+++ anonymisation.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import uuid
+
+
+df = pd.read_csv("employees.csv")
+
+
+
+df["Employee_Name"] = ["EMP_NUM" + str(i).zfill(4) for i in range(len(df))]
+df["ManagerName"] = "ANON_MANAGER"
+
+
+
+if "Zip" in df.columns:
+    
+    df = df.drop(columns=["Zip"])
+
+
+
+ethnic_columns = ["RaceDesc", "HispanicLatino"]
+
+for col in ethnic_columns:
+    if col in df.columns:
+        df[col] = "removed"
+
+
+df["DOB"] = pd.to_datetime(df["DOB"], errors="coerce").dt.year
+
+
+
+
+
+
+
+df.to_csv("employees_anonymized.csv", index=False)
